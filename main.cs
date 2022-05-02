5d5290748bf681876db1bff8273751467013c554
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-05-02 02:52:51 +0000

    done

--- main.cs
+++ main.cs
@@ -1,7 +1,25 @@
+// Created by: Jaejun Lee
+// Created on: April 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int userInteger;
+        Console.WriteLine("Put an integer:");
+        userInteger = int.Parse(Console.ReadLine());
+        if (userInteger > 0)
+        {
+            userInteger = Convert.ToInt32(Console.Read());
+            Console.WriteLine("The integer is positive.");
+        }
+        else
+        {
+            Console.WriteLine("The integer is negative.");
+        }
+    }
 }
